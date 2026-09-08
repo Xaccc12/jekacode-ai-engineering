68c1d13ed3b091f8a2ebbfe8d75b53b95b782856
Xaccc12 <david93452@gmail.com>
2026-09-08 10:42:09 -0700

    WEEK 2 PROJECT

---
+++ projects/DavidAjayi/ASSIGNMENT O2.py
@@ -0,0 +1,40 @@
+dct = {
+    'Daniel': 89,
+    'John': 49,
+    'Poju': 87,
+    'Jake': 74,
+    'David': 85,
+    'Nike': 43
+}
+def class_statistics(scores):
+    values = scores.values()
+
+    average = sum(values) / len(values)
+    highest = max(values)
+    lowest = min(values)
+
+    return average, highest, lowest
+
+
+average, highest, lowest = class_statistics(dct)
+
+print("Class Average:", average)
+print("Highest Score:", highest)
+print("Lowest Score:", lowest)
+
+for name, score in dct.items():
+    if score >= 50:
+        print(name, ":", score, "- Pass")
+    else:
+        print(name, ":", score, "- Fail")
+        prompt = f"""
+        The class average is {average} ,
+        The highest score is {highest},
+        The lowest score is {lowest}
+        
+        Write a one line paragraph summary of the class performance in plain english.
+        """
+
+        
+        
+
